94f9567e268c5ca0668db6f1a7e797f75f9ce443
vipul sharma <vipulsharmamtr@gmail.com>
2024-11-09 20:11:54 +0000

    comparison in java script

---
+++ 01_basics/comparison_operations.js
@@ -0,0 +1,12 @@
+// console.log(2 > 3) //false
+// console.log(2 < 3) // true
+// console.log(2 == 3) // false
+// console.log(2 != 3) //true
+
+// console.log("2" > 3) //false
+// console.log(2 < "3") // true
+// console.log(2 == "3") // false
+// console.log(2 === "2") // false
+
+console.log(null > 3) // false
+console.log(2 < null) // false
--- 01_basics/conversion_operationa.js
+++ 01_basics/conversion_operationa.js
@@ -1,17 +1,27 @@
 let score = 33
-// console.log(typeof(score));
+// // console.log(typeof(score));
 
-let stringScore = Boolean(score) // true
-console.log(typeof(stringScore)); 
+// let stringScore = Boolean(score) // true
+// console.log(typeof(stringScore)); 
 
-// let isMapped = true
-// console.log(typeof(isMapped));
+// // let isMapped = true
+// // console.log(typeof(isMapped));
 
-// console.log(stringScore);
+// // console.log(stringScore);
 
-let changed = Number(stringScore) // 1 (true => 1) 
-console.log(typeof(changed));
-console.log(changed);
+// let changed = Number(stringScore) // 1 (true => 1) 
+// console.log(typeof(changed));
+// console.log(changed);
+
+let sum = 1 + "1"
+console.log(sum);
+console.log(typeof sum);
+
+let sum1 = "1" + 1
+console.log(sum1);
+console.log(typeof sum1);
+
+
 
 
 
