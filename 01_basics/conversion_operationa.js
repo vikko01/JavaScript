84d9ed228dbf491cab46437f5d97734dd5c02ccb
vipul sharma <vipulsharmamtr@gmail.com>
2024-11-09 19:36:06 +0000

    conversion_operation

---
+++ 01_basics/conversion_operationa.js
@@ -0,0 +1,20 @@
+let score = 33
+// console.log(typeof(score));
+
+let stringScore = Boolean(score) // true
+console.log(typeof(stringScore)); 
+
+// let isMapped = true
+// console.log(typeof(isMapped));
+
+// console.log(stringScore);
+
+let changed = Number(stringScore) // 1 (true => 1) 
+console.log(typeof(changed));
+console.log(changed);
+
+
+
+
+
+
--- 01_basics/first.js
+++ 01_basics/first.js
@@ -1,1 +1,15 @@
 console.log("Ganpati Bappa Morya")
+
+const accId = 12345
+let accName = "sbi0123"
+var accMail = "vipul@123"
+accCity = "Jaipur"
+
+console.table([accId, accName, accMail, accCity])
+
+// accId = 45678 // cannot be changed because we described as an const
+accName = "pnb123" // it is change let affected with scope
+accMail = "vipul@567" // var cannot affect with scope
+accCity = "Bangeluru" // don't used it
+
+console.table([accId, accName, accMail, accCity])
